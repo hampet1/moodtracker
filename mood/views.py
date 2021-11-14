7bd96d1e414b85ef3646ad976568bc56d2200bc8
hampet1 <46238335+hampet1@users.noreply.github.com>
2021-11-14 19:16:52 +1100

    DL model implemented, do not forget to set relative paths to it!

--- mood/views.py
+++ mood/views.py
@@ -1,13 +1,66 @@
 from django.shortcuts import render
 from django import forms
+import sys
+
+from keras.models import model_from_json
 
 from .models import Message, Medication
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 # Create your views here.
-
-
 from django.template.defaulttags import register
+
+# other packages
+import os
+
+# text processing
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from tensorflow.keras.preprocessing.text import one_hot
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+
+weights_path = os.getcwd() + '\model.h5'
+model_path = os.getcwd() + '\model.json'
+
+
+
+
+def preprocessing(data):
+    ps = PorterStemmer()
+    corpus = []
+    data = data.split('.')
+    for i in data:
+        review = re.sub('[^a-zA-Z]', ' ', i)
+        review = review.lower()
+        review = review.split()
+        review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
+        review = ' '.join(review)
+        if review != '':
+            corpus.append(review)
+    return corpus
+
+
+def vectorize_sentence(corpus):
+    # Vocabulary size
+    voc_size=10000
+    onehot_repr=[one_hot(words,voc_size)for words in corpus]
+    # creating embedding layer as the input layer for our model we fixed lenght set to 20
+    sent_length=35
+    # pad sequence - we have to always input vector of the same size, but sentences are of
+    embedded_words=pad_sequences(onehot_repr,padding='pre',maxlen=sent_length)
+    return embedded_words
+
+def input_layer(data):
+    corpus = preprocessing(data)
+    embedded_input = vectorize_sentence(corpus)
+    return embedded_input
+
+
+
+
 
 @register.filter
 def get_range(value):
@@ -31,7 +84,6 @@ def index(request):
                       })
 
 
-
 def message(request):
     """
     pk and id is the same thing
@@ -49,22 +101,36 @@ def message(request):
         else:
             med_type = False
         rating = request.POST['rating']
-        #medication = request.POST['medication']
+        # medication = request.POST['medication']
         # user is the related name
         print("med type is", med_type)
         print("rating is: ", rating)
-        Message.objects.create(user=user, message=message)
-        return render(request, "mood/index.html",
-                      {
-                          'user':user.username,
-                          'message':Message.objects.get(pk=user.id)
-                          # pick possible medication
+        print("mesage is ", message)
 
+        # load json and create model
+        json_file = open(model_path, 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        # load weights into new model
+        loaded_model.load_weights(weights_path)
+        # evaluate loaded model on test data
+        loaded_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        text = input_layer(message)
+        # predictions
+        pred = (loaded_model.predict(text) > 0.5).astype("int32")
+        # our model prediction
+        print("our prediction is: ",pred)
+        Message.objects.create(user=user, message=message)
+        return render(request, "mood/mood_history.html",
+                      {
+                          'user': user.username,
+                          'message': Message.objects.get(pk=user.id)
+                          # pick possible medication
                       })
 
     else:
-        return render(request,'users/login.html')
-
+        return render(request, 'users/login.html')
 
 
 def mood_history(request):
@@ -84,7 +150,6 @@ def mood_history(request):
         pass
 
 
-
 def logout(request):
     logout(request)
     return render(request, "users/login.html",
@@ -92,5 +157,6 @@ def logout(request):
                       "message": "Logged out"
                   })
 
+
 def mood_boosts(request):
-    return render(request,"mood/mood_boost.html")
+    return render(request, "mood/mood_boost.html")
--- tracker/settings.py
+++ tracker/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+K_NAME = "JDKSJKDJS"
 
 # Application definition
 
