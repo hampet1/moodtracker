c9f77448281877a67f5889ad9fb1dceb0e718b7a
hampet1 <46238335+hampet1@users.noreply.github.com>
2021-12-31 19:43:37 +1100

    version for deployment prepared just work on details

--- mood/urls.py
+++ mood/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('mood_boost/', views.mood_boosts, name="mood_boost"),
     path('med/', views.medication_update, name="med"),
     path('med_del/', views.medication_delete, name="med_del"),
-    path('mood_history_result', views.mood_history_result, name="mood_history_result"),
-    path('download_pdf', views.download_pdf, name="download_pdf"),
-    path('guideline', views.guideline, name="guideline"),
+    path('mood_history_result/', views.mood_history_result, name="mood_history_result"),
+    path('download_pdf/', views.download_pdf, name="download_pdf"),
+    path('guideline/', views.guideline, name="guideline"),
 ]
