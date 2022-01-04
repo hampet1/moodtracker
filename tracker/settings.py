371e4a64b77784bfb415632a4caafca74ab97cb0
hampet1 <46238335+hampet1@users.noreply.github.com>
2022-01-04 12:57:46 +1100

    changed secret_key

--- tracker/settings.py
+++ tracker/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# for security purposes keep it as an environment variable
-SECRET_KEY = os.environ.get('DJANGO_KEY')
+
+SECRET_KEY = 'django-insecure-wye%quv3^@-=iyx90f=m#vbh8hr=35!)^5@)!l6$d_)66nr4f8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
