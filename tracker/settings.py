1bf29a303ecf22f5c26e9de6496b9ca9b308fd49
hampet1 <46238335+hampet1@users.noreply.github.com>
2022-01-06 18:45:16 +1100

    playing with settings

--- tracker/settings.py
+++ tracker/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # setting for deployment - heroku
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = '/mood/static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
